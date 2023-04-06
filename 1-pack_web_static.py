92589dec95d640a93492fe4642a5cf255364d570
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-04-06 18:48:01 +0300

    task1-tar-compression..with..try/except

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,10 +14,14 @@ def do_pack():
 
     Returns: The path of tar file.
     """
-    now_n = datetime.now()
+    now_n = datetime.utcnow()
     str_now = now_n.strftime("%Y%m%d%H%M%S")
+    file_c = "versions/web_static_{}.tgz".format(str_now)
 
-    local("mkdir -p versions")
-    local("tar -cvzf versions/web_static_{}.tar.gz web_static/".
-          format(str_now))
-    return "/versions/web_static_{}.tar.gz".format(str_now)
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf {} web_static/".format(file_c))
+        return file_c
+
+    except Exception:
+        return None
